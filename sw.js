52599b815be6b15e2f20cc9c6547166ad5cbe089
Marcus Green <mr.jr08@icloud.com>
2026-08-19 16:06:37 -0500

    Bump cache for finalized active workout fixes

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'level-up-fitness-20260819-active-workout-v26';
+const CACHE = 'level-up-fitness-20260819-active-workout-v27';
 const CORE = [
   './', './index.html', './app.css', './app.js', './supabase-config.js', './manifest.webmanifest',
   './theme.css', './navigation-simplify.css', './navigation-simplify.js',
