da75d6e00f14b6f4cdbfaba30f73a1a146c84031
Marcus Green <mr.jr08@icloud.com>
2026-08-18 21:03:59 -0500

    Refresh workout-specific quests build

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'level-up-fitness-20260819-profile-only-xp-v22';
+const CACHE = 'level-up-fitness-20260819-workout-quests-v23';
 const CORE = [
   './', './index.html', './app.css', './app.js', './supabase-config.js', './manifest.webmanifest',
   './theme.css', './navigation-simplify.css', './navigation-simplify.js',
