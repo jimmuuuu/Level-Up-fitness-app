c67fa20330374c4794956c62abc8752749058938
Marcus Green <mr.jr08@icloud.com>
2026-08-19 17:48:00 -0500

    Refresh expanded Scan recognition

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'level-up-fitness-20260819-scan-stability-v34';
+const CACHE = 'level-up-fitness-20260819-scan-any-equipment-v35';
 const CORE = [
   './', './index.html', './app.css', './app.js', './supabase-config.js', './manifest.webmanifest',
   './theme.css', './navigation-simplify.css', './navigation-simplify.js',
